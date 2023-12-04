6f8db59cc319a20630b1b867a064a9aec499e326
Shaun Williams <shaun@metabase.com>
2023-12-04 17:16:28 -0600

    split modals

--- frontend/src/metabase/admin/settings/components/ApiKeysList.tsx
+++ frontend/src/metabase/admin/settings/components/ApiKeysList.tsx
@@ -8,78 +8,106 @@ import { ApiKeysApi } from "metabase/services";
 import { Icon } from "metabase/core/components/Icon";
 import { Form, FormProvider } from "metabase/forms";
 
+export const CreateApiKeyModal = ({ handleClose }) => {
+  return (
+    <Modal
+      padding="xl"
+      opened
+      onClose={handleClose}
+      title={t`Create a new API Key`}
+    />
+  );
+};
+
+export const EditApiKeyModal = ({ handleClose }) => {
+  return (
+    <Modal padding="xl" opened onClose={handleClose} title={t`Edit API Key`} />
+  );
+};
+
+export const DeleteApiKeyModal = ({
+  handleClose,
+  handleRefresh,
+  activeRow,
+}) => {
+  return (
+    <Modal
+      size="30rem"
+      padding="xl"
+      opened
+      onClose={handleClose}
+      title={t`Delete API Key`}
+    >
+      <Stack>
+        <p>{t`API key deleted can’t be recovered. You have to create a new key.`}</p>
+        <Group position="right">
+          <Button
+            color="error.0"
+            onClick={handleClose}
+          >{t`No, don’t delete`}</Button>
+          <Button
+            variant="filled"
+            color="error.0"
+            onClick={async () => {
+              await ApiKeysApi.delete({ id: activeRow.id });
+              handleClose();
+              handleRefresh();
+            }}
+          >{t`Delete API Key`}</Button>
+        </Group>
+      </Stack>
+    </Modal>
+  );
+};
+
+const MOCK_ROWS = [
+  {
+    name: "Development API Key",
+    id: 1,
+    group_id: 1,
+    creator_id: 1,
+    masked_key: "asdfasdfa",
+    created_at: "2010 Aug 10",
+    updated_at: "2010 Aug 10",
+  },
+  {
+    name: "Production API Key",
+    id: 2,
+    group_id: 1,
+    creator_id: 1,
+    masked_key: "asdfasdfa",
+    created_at: "2010 Aug 10",
+    updated_at: "2010 Aug 10",
+  },
+];
+
 export const ApiKeysList = () => {
   const [keyRows, setKeyRows] = useState([]);
   const [isCreating, setIsCreating] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
   const [activeRow, setActiveRow] = useState(null);
+  const handleRefresh = () => ApiKeysApi.list().then(setKeyRows);
 
   useEffect(() => {
     // :name, :group_id, :created_at, :updated_at, and :masked_key
     ApiKeysApi.list().then(setKeyRows);
-    setKeyRows([
-      {
-        name: "Development API Key",
-        id: 1,
-        group_id: 1,
-        creator_id: 1,
-        masked_key: "asdfasdfa",
-        created_at: "2010 Aug 10",
-        updated_at: "2010 Aug 10",
-      },
-      {
-        name: "Production API Key",
-        id: 2,
-        group_id: 1,
-        creator_id: 1,
-        masked_key: "asdfasdfa",
-        created_at: "2010 Aug 10",
-        updated_at: "2010 Aug 10",
-      },
-    ]);
+    setKeyRows(MOCK_ROWS);
   }, []);
 
   return (
     <>
-      <Modal
-        padding="xl"
-        opened={isCreating}
-        onClose={() => setIsCreating(false)}
-        title={t`Create a new API Key`}
-      />
-      <Modal
-        padding="xl"
-        opened={isEditing}
-        onClose={() => setIsEditing(false)}
-        title={t`Edit API Key`}
-      />
-      <Modal
-        size="30rem"
-        padding="xl"
-        opened={isDeleting}
-        onClose={() => setIsDeleting(false)}
-        title={t`Delete API Key`}
-      >
-        <Stack>
-          <p>{t`API key deleted can’t be recovered. You have to create a new key.`}</p>
-          <Group position="right">
-            <Button
-              color="error.0"
-              onClick={() => setIsDeleting(false)}
-            >{t`No, don’t delete`}</Button>
-            <Button
-              variant="filled"
-              color="error.0"
-              onClick={async () => {
-                await ApiKeysApi.delete({ id: activeRow.id });
-                setIsDeleting(false);
-                ApiKeysApi.list().then(setKeyRows);
-              }}
-            >{t`Delete API Key`}</Button>
-          </Group>
-        </Stack>
-      </Modal>
+      {isCreating ? (
+        <CreateApiKeyModal handleClose={() => setIsCreating(false)} />
+      ) : isEditing ? (
+        <EditApiKeyModal handleClose={() => setIsEditing(false)} />
+      ) : isDeleting ? (
+        <DeleteApiKeyModal
+          activeRow={activeRow}
+          handleRefresh={handleRefresh}
+          handleClose={() => setIsDeleting(false)}
+        />
+      ) : null}
       <Stack pl="md">
         <Breadcrumbs
           className="mb3"
