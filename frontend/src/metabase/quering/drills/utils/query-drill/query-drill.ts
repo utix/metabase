f1ff5f46c39eb96a346097ffe67d9ba1d4a4980f
Alexander Polyankin <alexander.polyankin@metabase.com>
2023-12-04 13:06:23 +0200

    Move query drills

--- frontend/src/metabase/quering/drills/utils/query-drill/query-drill.ts
+++ frontend/src/metabase/quering/drills/utils/query-drill/query-drill.ts
@@ -1,18 +1,21 @@
+import type { ClickAction } from "metabase/visualizations/types";
 import * as Lib from "metabase-lib";
-import type { ClickObject } from "metabase-lib/queries/drills/types";
 import type Question from "metabase-lib/Question";
 import { DRILLS } from "./constants";
 
-export function queryDrill(question: Question, clicked: ClickObject) {
+export function queryDrill(
+  question: Question,
+  clicked?: Lib.ClickObject,
+): ClickAction[] {
   const query = question._getMLv2Query();
   const stageIndex = -1;
   const drills = Lib.availableDrillThrus(
     query,
     stageIndex,
-    clicked.column,
-    clicked.value,
-    clicked.data,
-    clicked.dimensions,
+    clicked?.column,
+    clicked?.value,
+    clicked?.data,
+    clicked?.dimensions,
   );
 
   const applyDrill = (drill: Lib.DrillThru, ...args: unknown[]) => {
