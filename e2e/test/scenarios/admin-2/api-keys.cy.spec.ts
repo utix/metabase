961be0beff49724752a7da362a394ed9edbe82dd
Shaun Williams <shaun@metabase.com>
2023-12-14 17:40:32 -0600

    add e2e spec for editing key

--- e2e/test/scenarios/admin-2/api-keys.cy.spec.ts
+++ e2e/test/scenarios/admin-2/api-keys.cy.spec.ts
@@ -24,8 +24,13 @@ const MOCK_ROWS = [
   },
 ];
 
+const getRequestId = req =>
+  parseInt(req.url.match(/api-key\/(\d+)/)?.[1] ?? "", 10);
+
 describe("scenarios > admin > settings > API keys", () => {
+  // TODO: replace intercepts below with actual requests to test backend
   let mockRows: ApiKey[] = [];
+
   beforeEach(() => {
     restore();
     mockRows = [...MOCK_ROWS];
@@ -33,7 +38,6 @@ describe("scenarios > admin > settings > API keys", () => {
     // @ts-ignore
     cy.signInAsAdmin();
   });
-  // TODO: replace intercepts below with actual requests to test backend
 
   it("should show number of API keys on auth card", () => {
     cy.intercept("GET", "/api/api-key/count", req => req.reply(200, "5"));
@@ -53,7 +57,7 @@ describe("scenarios > admin > settings > API keys", () => {
     getApiKeysCard().findByTestId("card-badge").should("not.exist");
   });
   it("should list existing API keys", () => {
-    cy.intercept("GET", "/api/api-key", req => req.reply(200, MOCK_ROWS));
+    cy.intercept("GET", "/api/api-key", req => req.reply(200, mockRows));
     cy.visit("/admin/settings/authentication/api-keys");
     getApiKeysRows()
       .should("contain", "Development API Key")
@@ -86,7 +90,7 @@ describe("scenarios > admin > settings > API keys", () => {
       "fetchKeys",
     );
     cy.intercept("DELETE", "/api/api-key/*", req => {
-      const id = parseInt(req.url.match(/api-key\/(\d+)/)?.[1] ?? "", 10);
+      const id = getRequestId(req);
       mockRows = mockRows.filter(row => row.id !== id);
       req.reply(200);
     }).as("deleteKey");
@@ -105,7 +109,36 @@ describe("scenarios > admin > settings > API keys", () => {
     getApiKeysRows().should("not.contain", "Development API Key");
   });
   it("should allow editing an API key", () => {
-    //
+    cy.intercept("GET", "/api/api-key", req => req.reply(200, mockRows)).as(
+      "fetchKeys",
+    );
+    cy.intercept("PUT", "/api/api-key/*", req => {
+      const id = getRequestId(req);
+      const rowI = mockRows.findIndex(row => row.id === id);
+      const row = mockRows[rowI];
+      mockRows[rowI] = { ...row, ...req.body };
+      req.reply(200);
+    }).as("saveKey");
+    cy.visit("/admin/settings/authentication/api-keys");
+    cy.wait("@fetchKeys");
+    getApiKeysRows()
+      .contains("Development API Key")
+      .closest("tr")
+      .icon("pencil")
+      .click();
+    cy.findByLabelText(/Key name/)
+      .clear()
+      .type("Different key name");
+    cy.button("Save").click();
+    cy.wait("@saveKey");
+    cy.wait("@fetchKeys");
+    getApiKeysRows()
+      .should("not.contain", "Development API Key")
+      .and("contain", "Different key name");
+    cy.reload();
+    getApiKeysRows()
+      .should("not.contain", "Development API Key")
+      .and("contain", "Different key name");
   });
   it("should be notified when deleting a group with API keys", () => {
     //
