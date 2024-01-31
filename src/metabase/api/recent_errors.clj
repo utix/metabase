7a6aae10a09aefe065091858c519d8aa06ffddd2
John Swanson <john.swanson@metabase.com>
2024-01-31 14:18:06 -0500

    record a *real* exception. wow!

--- src/metabase/api/recent_errors.clj
+++ src/metabase/api/recent_errors.clj
@@ -1,9 +1,23 @@
 (ns metabase.api.recent-errors
-  (:require [compojure.core :refer [GET]]
+  (:require [amalloy.ring-buffer :as ring-buffer]
+            [compojure.core :refer [GET]]
             [metabase.api.common :as api]))
+
+(set! *warn-on-reflection* true)
+
+(def ^:private recent-errors (atom (ring-buffer/ring-buffer 10)))
+
+(defn record!
+  "Records an exception and the time it occurred so we can tell the frontend about it later."
+  [e]
+  (swap! recent-errors conj {:error e
+                             :occurred-at (java.time.Instant/now)}))
+
+;; record an exception for dev
+(record! (ex-info "meow mix meow mix we deliver" {}))
 
 (api/defendpoint GET "/recent"
   []
-  [{:message "my happy error"}])
+  (map (fn [m] (update m :error #(.getMessage ^Throwable %))) @recent-errors))
 
 (api/define-routes)
