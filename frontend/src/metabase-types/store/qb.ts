f6fc30b4e4dd601a416907f39994d43f598ad540
Oisin Coveney <oisin@metabase.com>
2024-10-15 10:28:14 +0300

    fix another error

--- frontend/src/metabase-types/store/qb.ts
+++ frontend/src/metabase-types/store/qb.ts
@@ -1,10 +1,13 @@
 import type { QueryModalType } from "metabase/query_builder/constants";
+import type Question from "metabase-lib/v1/Question";
+import type Database from "metabase-lib/v1/metadata/Database";
+import type Field from "metabase-lib/v1/metadata/Field";
+import type Schema from "metabase-lib/v1/metadata/Schema";
+import type Table from "metabase-lib/v1/metadata/Table";
 import type {
   Card,
   DashboardId,
-  Database,
   Dataset,
-  Field,
   ParameterValueOrArray,
 } from "metabase-types/api";
 
@@ -16,6 +19,15 @@ export type ForeignKeyReference = {
   status: number;
   value: number;
 };
+
+export type DataReferenceStackItem =
+  | { type: "database"; item: Pick<Database, "id"> }
+  | { type: "schema"; item: Pick<Schema, "id"> }
+  | { type: "table"; item: Pick<Table, "id"> }
+  | { type: "question"; item: Pick<Question, "id"> }
+  | { type: "field"; item: Field };
+
+export type DataReferenceStack = DataReferenceStackItem[];
 
 export interface QueryBuilderUIControls {
   isModifiedFromNotebook: boolean;
@@ -41,7 +53,7 @@ export interface QueryBuilderUIControls {
   notebookNativePreviewSidebarWidth: number | null;
   scrollToLastColumn?: boolean;
   modal: QueryModalType | null;
-  dataReferenceStack: { type: "database"; item: Database }[] | null;
+  dataReferenceStack: DataReferenceStack | null;
   initialChartSettings: { section: string };
   showSidebarTitle: boolean;
 }
