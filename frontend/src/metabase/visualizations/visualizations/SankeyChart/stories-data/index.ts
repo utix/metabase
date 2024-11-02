2af555a31c37d9c008d062d9f8300ffd83cc2a52
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-11-01 21:07:03 -0400

    add sankey chart visual tests

---
+++ frontend/src/metabase/visualizations/visualizations/SankeyChart/stories-data/index.ts
@@ -0,0 +1,15 @@
+import sankeyDisconnectedGraphs from "./sankey-disconnected-graphs.json";
+import sankeyNodeAlignJustify from "./sankey-node-align-justify.json";
+import sankeyNodeAlignLeft from "./sankey-node-align-left.json";
+import sankeyNodeAlignRight from "./sankey-node-align-right.json";
+import sankeyUnaggregatedData from "./sankey-unaggregated-data.json";
+import sankeyWithEdgesLabels from "./sankey-with-edges-labels.json";
+
+export {
+  sankeyDisconnectedGraphs,
+  sankeyNodeAlignJustify,
+  sankeyNodeAlignLeft,
+  sankeyNodeAlignRight,
+  sankeyUnaggregatedData,
+  sankeyWithEdgesLabels,
+};
